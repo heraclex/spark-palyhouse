df878326dd001e776edc4038524d4d5180532bce
heraclex <toan.leebao@gmail.com>
2022-04-19 11:24:57 +0200

    refactoring

--- spark-delta/src/main/scala/com/playhouse/delta/main/WriteToDeltaMain.scala
+++ spark-delta/src/main/scala/com/playhouse/delta/main/WriteToDeltaMain.scala
@@ -19,28 +19,27 @@ object WriteToDeltaMain {
       val tableCols =
         s"""travelCode int, userCode int, name string, place string, stayingDays int,
            |price float, total float, `date` date, day int""".stripMargin
+      val query = s"""
+                     |select cast(travelCode as int) as travelCode,
+                     |cast(userCode as int) as userCode,
+                     |name,place,
+                     |cast(days as int) as stayingDays,
+                     |cast(price as float) as price,
+                     |cast(total as float) as total,
+                     |to_date(date, 'MM/dd/yyyy') as date,
+                     |cast(date_format(to_date(date, 'MM/dd/yyyy'), 'yyyyMMdd') as int) as day
+                     |from tmp""".stripMargin
 
       // https://github.com/delta-io/connectors/issues/71
       spark.read.option("delimiter", ",")
-        .option("header", "true").csv("./src/main/resources/hotels_part4.csv")
+        .option("header", "true").csv("./src/main/resources/hotels_part1.csv")
         .createTempView("tmp")
 
-      val df = spark.sql(
-        s"""
-           |select cast(travelCode as int) as travelCode,
-           |cast(userCode as int) as userCode,
-           |name,place,
-           |cast(days as int) as stayingDays,
-           |cast(price as float) as price,
-           |cast(total as float) as total,
-           |to_date(date, 'MM/dd/yyyy') as date,
-           |cast(date_format(to_date(date, 'MM/dd/yyyy'), 'yyyyMMdd') as int) as day
-           |from tmp
-           |""".stripMargin)
+      val df = spark.sql(query)
 
       df.show(10,true)
 
-      spark.sql(s"CREATE DATABASE IF NOT EXISTS $databaseName LOCATION 's3a://${TargetSystem.DELTA.toString}/$databaseName.db'".stripMargin)
+      spark.sql(s"CREATE DATABASE IF NOT EXISTS $databaseName LOCATION '$s3databaseLocation'".stripMargin)
       df.write.format("delta").partitionBy("day")
         .mode("append").saveAsTable(s"$databaseName.$tableName")
 
